58bb431129674b26fe3774be6a40d7223b15b9b6
Benoît Dardenne <benoit.dardenne@gmail.com>
2014-11-04 09:11:54 +0100

    Basic Agent first commit

---
+++ basic_agent.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Zombies agent.
+Copyright (C) 2014, <<<<<<<<<<< YOUR NAMES HERE >>>>>>>>>>>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import zombies
+import minimax
+
+class Agent: #(Agent, minimax.Game):
+    """This is the skeleton of an agent to play the Zombies game."""
+
+    def __init__(self, name="Basic Agent"):
+        self.name = name
+        self.player = zombies.PLAYER2
+
+    """The successors function must return (or yield) a list of
+    pairs (a, s) in which a is the action played to reach the
+    state s; s is the new state, i.e. a triplet (b, p, st) where
+    b is the new board after the action a has been played,
+    p is the player to play the next move and st is the next
+    step number.
+    """
+    def successors(self, state):
+        b, p, st = state
+        for a in b.get_actions(p, st):
+            newboard = b.clone()
+            newboard.play_action(a, p, st)
+            yield (a, (newboard, -p, st + 1))      
+
+    """The cutoff function returns true if the alpha-beta/minimax
+    search has to stop; false otherwise.
+    """
+    def cutoff(self, state, depth):
+        if depth > 2 or state[0].is_finished():
+            return True
+
+        return False
+
+    """The evaluate function must return an integer value
+    representing the utility function of the board.
+    """
+    def evaluate(self, state):
+        score = state[0].get_score()
+        if score > 0:
+            return 1
+        elif score < 0:
+            return -1
+        return 0
+
+    def play(self, board, player, step, time_left):
+        """This function is used to play a move according
+        to the board, player and time left provided as input.
+        It must return an action representing the move the player
+        will perform.
+        """
+        self.player = player
+        self.time_left = time_left
+        state = (board, player, step)
+        return minimax.search(state, self)
+
+
+if __name__ == "__main__":
+    zombies.agent_main(Basic_Agent())
